dae6fb36743ed0cfab04f06f0962db77dc66272f
Neel Ismail <neelismail01@gmail.com>
2021-04-08 22:17:56 -0400

    order items being retrieved properly

--- backend/models/order.js
+++ backend/models/order.js
@@ -6,8 +6,9 @@ const orderSchema = mongoose.Schema({
         ref: 'Business',
     },
     orderItems: [{
-        type: mongoose.Schema.Types.Mixed,
-        required:true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
     }],
     shippingAddress1: {
         type: String,
@@ -51,30 +52,3 @@ orderSchema.set('toJSON', {
 });
 
 exports.Order = mongoose.model('Order', orderSchema);
-
-
-
-/**
-Order Example:
-
-{
-    "orderItems" : [
-        {
-            "quantity": 3,
-            "product" : "5fcfc406ae79b0a6a90d2585"
-        },
-        {
-            "quantity": 2,
-            "product" : "5fd293c7d3abe7295b1403c4"
-        }
-    ],
-    "shippingAddress1" : "Flowers Street , 45",
-    "shippingAddress2" : "1-B",
-    "city": "Prague",
-    "zip": "00000",
-    "country": "Czech Republic",
-    "phone": "+420702241333",
-    "user": "5fd51bc7e39ba856244a3b44"
-}
-
- */
